5862ce7f39daa5772192636320c1bf053535f251
Matias-Barbosa <105323943+Matias-Barbosa@users.noreply.github.com>
2022-06-08 18:29:54 -0300

    Update PedidoModel.java

--- src/main/java/br/com/triersistemas/sonar/Model/PedidoModel.java
+++ src/main/java/br/com/triersistemas/sonar/Model/PedidoModel.java
@@ -1,4 +1,15 @@
 package br.com.triersistemas.sonar.Model;
 
+import lombok.Getter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
 public class PedidoModel {
+    private UUID id;
+    private String nome;
+    private int valor;
+    private String cliente;
+    private List<String> listaPedidos;
 }
